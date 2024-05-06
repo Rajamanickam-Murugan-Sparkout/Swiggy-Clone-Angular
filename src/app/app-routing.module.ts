46dc0f09dfb600f5af3274cdab86f538487362ed
Rajamanickam <rajamanickam@sparkouttech.com>
2024-05-06 12:39:32 +0530

    nav-restarant-pages-completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopRestaurantComponent } from './Body/top-restaurant/top-restaurant.component';
+import { PizzaHutComponent } from './Body/restaurants/pizza-hut/pizza-hut.component';
+import { BarbequeComponent } from './Body/restaurants/barbeque/barbeque.component';
+import { BluemoonComponent } from './Body/restaurants/bluemoon/bluemoon.component';
+import { GuptaComponent } from './Body/restaurants/gupta/gupta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: TopRestaurantComponent},
+  {path:'pizza-hut', component: PizzaHutComponent},
+  {path:'barbeque', component: BarbequeComponent},
+  {path:'bluemoon', component: BluemoonComponent},
+  {path:'gupta', component: GuptaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Header/navbar/navbar.component';
+import { TopRestaurantComponent } from './Body/top-restaurant/top-restaurant.component';
+import { PizzaHutComponent } from './Body/restaurants/pizza-hut/pizza-hut.component';
+import { BarbequeComponent } from './Body/restaurants/barbeque/barbeque.component';
+import { BluemoonComponent } from './Body/restaurants/bluemoon/bluemoon.component';
+import { GuptaComponent } from './Body/restaurants/gupta/gupta.component';
+import { AddToCartComponent } from './Body/cart/add-to-cart/add-to-cart.component';
+import { CommonModule } from '@angular/common';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TopRestaurantComponent,
+    PizzaHutComponent,
+    BarbequeComponent,
+    BluemoonComponent,
+    GuptaComponent,
+    AddToCartComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
